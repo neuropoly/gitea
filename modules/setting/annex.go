ec23e25e5bcc63cf15b18b5e923c939f6c983af8
Nick <nick.guenther@polymtl.ca>
2022-09-19 17:22:42 -0400

    git-annex: add configuration setting [annex].ENABLED (#18)
    Fixes https://github.com/neuropoly/gitea/issues/8
    
    Co-authored-by: Mathieu Guay-Paquet <mathieu.guaypaquet@gmail.com>

---
+++ modules/setting/annex.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The Gitea Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package setting
+
+import (
+	"code.gitea.io/gitea/modules/log"
+)
+
+// Annex represents the configuration for git-annex
+var Annex = struct {
+	Enabled bool `ini:"ENABLED"`
+}{}
+
+func loadAnnexFrom(rootCfg ConfigProvider) {
+	sec := rootCfg.Section("annex")
+	if err := sec.MapTo(&Annex); err != nil {
+		log.Fatal("Failed to map Annex settings: %v", err)
+	}
+}
