5da659117d8f5f0170b12ffe5037a9d2fc8aae76
Nick Guenther <nick.guenther@polymtl.ca>
2022-11-27 03:54:24 -0500

    cornerstone: display NIfTI and dicom image previews

--- web_src/js/index.js
+++ web_src/js/index.js
@@ -89,6 +89,7 @@ import {initRepoWikiForm} from './features/repo-wiki.js';
 import {initRepoCommentForm, initRepository} from './features/repo-legacy.js';
 import {initFormattingReplacements} from './features/formatting.js';
 import {initMcaptcha} from './features/mcaptcha.js';
+import {initCornerstone} from './features/cornerstone.js';
 
 // Run time-critical code as soon as possible. This is safe to do because this
 // script appears at the end of <body> and rendered HTML is accessible at that point.
@@ -188,6 +189,7 @@ $(document).ready(() => {
 
   initCommitStatuses();
   initMcaptcha();
+  initCornerstone();
 
   initUserAuthLinkAccountView();
   initUserAuthOauth2();
